3b3e82543b59c9f3039c4d72b3b7e59251bd1f23
Sakshi Kusmude <sakshikusmude699@gmail.com>
2026-01-14 00:13:22 +0530

    Fix NLTK stopwords issue for deployment

--- utils/text_cleaner.py
+++ utils/text_cleaner.py
@@ -3,24 +3,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-stop_words = set(stopwords.words("english"))
-
-def clean_text(text):
-    # Lowercase
-    text = text.lower()
-
-    # Remove emails, links, numbers
-    text = re.sub(r'\S+@\S+', ' ', text)
-    text = re.sub(r'http\S+', ' ', text)
-    text = re.sub(r'\d+', ' ', text)
-
-    # Remove special characters
-    text = re.sub(r'[^a-zA-Z\s]', ' ', text)
-
-    # Tokenization
-    tokens = word_tokenize(text)
-
-    # Remove stopwords
-    cleaned_tokens = [word for word in tokens if word not in stop_words]
-
-    return " ".join(cleaned_tokens)
+try:
+    stop_words = set(stopwords.words("english"))
+except LookupError:
+    nltk.download("stopwords")
+    nltk.download("punkt")
+    stop_words = set(stopwords.words("english"))
