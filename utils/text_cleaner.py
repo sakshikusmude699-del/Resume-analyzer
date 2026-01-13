4e7636a0a2d44f805a6474a8f4836d66d22a781b
Sakshi Kusmude <sakshikusmude699@gmail.com>
2026-01-14 00:26:50 +0530

    Fix clean_text import error

--- utils/text_cleaner.py
+++ utils/text_cleaner.py
@@ -1,11 +1,22 @@
 import re
 import nltk
+import os
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-try:
-    stop_words = set(stopwords.words("english"))
-except LookupError:
-    nltk.download("stopwords")
-    nltk.download("punkt")
-    stop_words = set(stopwords.words("english"))
+nltk.data.path.append(os.path.join(os.getcwd(), "nltk_data"))
+
+stop_words = set(stopwords.words("english"))
+
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\S+@\S+', ' ', text)
+    text = re.sub(r'http\S+', ' ', text)
+    text = re.sub(r'\d+', ' ', text)
+    text = re.sub(r'[^a-zA-Z\s]', ' ', text)
+
+    tokens = word_tokenize(text)
+    cleaned_tokens = [word for word in tokens if word not in stop_words]
+
+    return " ".join(cleaned_tokens)
